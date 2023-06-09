4cfe3c7586771c785ee35458b423b06799bc270a
krrn0722 <mur0722@outlook.jp>
2023-06-09 13:38:59 +0900

    add: futureProviderでAPIから取得する処理を実装

--- lib/view_model/providers.dart
+++ lib/view_model/providers.dart
@@ -1,3 +1,15 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:wheather/Repository/repository.dart';
+import 'package:wheather/models/weather.dart';
 
 final cityNameProvider = StateProvider((ref) => 'Tokyo');
+
+final repostitoryProvider = Provider((ref) => Repository());
+
+// APIの取得
+final dataProvider = FutureProvider.autoDispose<Weather>((ref) async {
+  final repository = ref.read(repostitoryProvider);
+  final cityName = ref.watch(cityNameProvider.notifier);
+  // 都市名を組み込み、APIを取得する
+  return await repository.fetchWeather(cityName.state);
+});
